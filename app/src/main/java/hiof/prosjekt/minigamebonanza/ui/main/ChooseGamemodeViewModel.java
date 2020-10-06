9743e498f66641b9088468ee8f2cbfd82b2610fc
Tomzing <thomas_b_e@hotmail.com>
2020-10-06 22:26:22 +0200

    Initial commit

---
+++ app/src/main/java/hiof/prosjekt/minigamebonanza/ui/main/ChooseGamemodeViewModel.java
@@ -0,0 +1,7 @@
+package hiof.prosjekt.minigamebonanza.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class ChooseGamemodeViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/hiof/prosjekt/minigamebonanza/ui/main/SettingsFragment.java
@@ -0,0 +1,15 @@
+package hiof.prosjekt.minigamebonanza.ui.main;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+import hiof.prosjekt.minigamebonanza.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+}
